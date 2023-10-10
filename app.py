fcf4fac291026ca24e35ca3377ccee703ee2c36b
priyanka-575 <priyankakokil575@gmail.com>
2023-10-09 21:52:13 -0400

    Model changes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import os
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
 import models
 from flask_migrate import Migrate
 from resources.user import blp as UserBlueprint
@@ -12,6 +14,7 @@ from flask import Flask, jsonify
 from blocklist import BLOCKLIST
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
@@ -20,7 +23,7 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
--- migrations/env.py
+++ migrations/env.py
@@ -65,7 +65,7 @@ def run_migrations_offline():
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url, target_metadata=get_metadata(), literal_binds=True
+        url=url, target_metadata=get_metadata(), compare_type=True, literal_binds=True
     )
 
     with context.begin_transaction():
@@ -99,6 +99,7 @@ def run_migrations_online():
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
+            #compare_type=True,
             target_metadata=get_metadata(),
             **conf_args
         )
